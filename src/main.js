6eeaa3d29c2a22e47d3d31594a1af997776029a4
Karen Tapia <karenina.tap@gmail.com>
2021-01-22 10:55:43 -0600

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
-import { example } from './data.js';
+/* import { example } from './data.js';
 // import data from './data/lol/lol.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
 
 
-console.log(example, data);
+console.log(example, data); */
+
+import dataRM from './data/rickandmorty.js';
+// import data from './data/lol/lol.js';
+document.addEventListener('DOMContentLoaded',function(){
+    elhtml(dataRM.results)
+    
+})
+let nodoPersonaje=document.querySelector('.character')
+
+function elhtml(losdatos){
+    
+    losdatos.forEach(unPersonaje => {
+        let parrafo=document.createElement('p')
+        parrafo.textContent=`${unPersonaje.name}` //aqui usar inner html con los personaje
+        nodoPersonaje.appendChild(parrafo)
+    });
+
+}
